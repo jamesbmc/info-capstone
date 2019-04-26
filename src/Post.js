378b16589d909778c856fe7924f91536ec62ce6e
jamesbmc <jbmc@uw.edu>
2019-04-26 16:21:11 -0700

    adding more to voting

--- src/Post.js
+++ src/Post.js
@@ -1,9 +1,18 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom'
 import upvote from './upvote.png';
+import upvoteDisabled from './upvoteDisabled.png';
 import downvote from './downvote.png';
+import downvoteDisabled from './downvoteDisabled.png';
+import firebase from 'firebase/app';
 
 export class Post extends Component {
+    handleVote(action) {
+        if (action === "up") {
+
+        }
+    }
+
     render() {
         let upvotes = typeof this.props.info.upvotes === "undefined" ? 0 : this.props.info.upvotes.length;
         let downvotes = typeof this.props.info.downvotes === "undefined" ? 0 : this.props.info.downvotes.length;
@@ -20,9 +29,11 @@ export class Post extends Component {
                     }
                  }} style={{ textDecoration: 'none' }}>
                     <div align='left' style={{backgroundColor: '#eee', padding: '1rem'}}>
-                        <img src={upvote} style={{ width: '3%', color: "green" }} alt="Upvote icon" />
+                        {(typeof this.props.info.upvotes === "undefined" || !this.props.info.upvotes.includes(firebase.auth().currentUser.uid)) && <img src={upvote} style={{ width: '3%' }} alt="Upvote icon" onClick={() => this.handleVote("up")}/>}
+                        {(typeof this.props.info.upvotes !== "undefined" && this.props.info.upvotes.includes(firebase.auth().currentUser.uid)) && <img src={upvoteDisabled} style={{ width: '3%' }} alt="Upvote icon (disabled)" />}
                         <p style={{display: 'inline', color: total >= 0 ? 'green' : 'red'}}>{total}</p>
-                        <img src={downvote} style={{ width: '3%' }} alt="Downvote icon" />
+                        {(typeof this.props.info.downvotes === "undefined" || !this.props.info.downvotes.includes(firebase.auth().currentUser.uid)) && <img src={downvote} style={{ width: '3%' }} alt="Downvote icon" onClick={() => this.handleVote("down")} />}
+                        {(typeof this.props.info.downvotes !== "undefined" && this.props.info.downvotes.includes(firebase.auth().currentUser.uid)) && <img src={downvoteDisabled} style={{ width: '3%' }} alt="Downvote icon (disabled)" />}
                         <h3 style={{display: 'inline', 'marginLeft': '3rem', color:'black'}}>{this.props.info.title}</h3>
                         <p style={{display: 'inline', 'float': 'right', color:'black'}}>{this.props.info.author + ", " + this.props.info.date}</p>
                         {this.props.username === this.props.info.author && <p>this will be a delete button</p>}
