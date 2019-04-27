bf625ab4c81aa410adc52c883e6d843146ec480a
jamesbmc <jbmc@uw.edu>
2019-04-26 17:03:06 -0700

    add voiding of past votes, fix bugs

--- src/Post.js
+++ src/Post.js
@@ -11,24 +11,29 @@ export class Post extends Component {
     handleVote(e, action) {
         e.preventDefault();
         if (action === "up") {
-            console.log("up");
             if (typeof this.props.info.downvotes !== "undefined" && this.props.info.downvotes.includes(firebase.auth().currentUser.uid)) {
                 let newDownvotes = this.props.info.downvotes.filter(id => id !== firebase.auth().currentUser.uid);
-                this.infoRef.update({upvotes: newDownvotes});
+                firebase.database().ref('posts/' + this.props.info.id).update({downvotes: newDownvotes});
             }
-            this.infoRef.update({upvotes: typeof this.props.info.upvotes === "undefined" ? [firebase.auth().currentUser.uid] : this.props.info.upvotes.concat(firebase.auth().currentUser.uid)});
+            firebase.database().ref('posts/' + this.props.info.id).update({upvotes: typeof this.props.info.upvotes === "undefined" ? [firebase.auth().currentUser.uid] : this.props.info.upvotes.concat(firebase.auth().currentUser.uid)});
         } else {
-            console.log("down");
             if (typeof this.props.info.upvotes !== "undefined" && this.props.info.upvotes.includes(firebase.auth().currentUser.uid)) {
                 let newUpvotes = this.props.info.upvotes.filter(id => id !== firebase.auth().currentUser.uid);
-                this.infoRef.update({upvotes: newUpvotes});
+                firebase.database().ref('posts/' + this.props.info.id).update({upvotes: newUpvotes});
             }
-            this.infoRef.update({downvotes: typeof this.props.info.downvotes === "undefined" ? [firebase.auth().currentUser.uid] : this.props.info.downvotes.concat(firebase.auth().currentUser.uid)});
+            firebase.database().ref('posts/' + this.props.info.id).update({downvotes: typeof this.props.info.downvotes === "undefined" ? [firebase.auth().currentUser.uid] : this.props.info.downvotes.concat(firebase.auth().currentUser.uid)});
         }
     }
 
-    componentDidMount() {
-        this.infoRef = firebase.database().ref('posts/' + this.props.info.id);
+    voidVote(e, action) {
+        e.preventDefault();
+        if (action === "up") {
+            let newUpvotes = this.props.info.upvotes.filter(id => id !== firebase.auth().currentUser.uid);
+            firebase.database().ref('posts/' + this.props.info.id).update({upvotes: newUpvotes});
+        } else {
+            let newDownvotes = this.props.info.downvotes.filter(id => id !== firebase.auth().currentUser.uid);
+            firebase.database().ref('posts/' + this.props.info.id).update({downvotes: newDownvotes});
+        }
     }
 
     render() {
@@ -48,10 +53,10 @@ export class Post extends Component {
                  }} style={{ textDecoration: 'none' }}>
                     <div align='left' style={{backgroundColor: '#eee', padding: '1rem'}}>
                         {(typeof this.props.info.upvotes === "undefined" || !this.props.info.upvotes.includes(firebase.auth().currentUser.uid)) && <img src={upvote} style={{ width: '3%' }} alt="Upvote icon" onClick={(e) => this.handleVote(e, "up")}/>}
-                        {(typeof this.props.info.upvotes !== "undefined" && this.props.info.upvotes.includes(firebase.auth().currentUser.uid)) && <img src={upvoteDisabled} style={{ width: '3%' }} alt="Upvote icon (disabled)" />}
+                        {(typeof this.props.info.upvotes !== "undefined" && this.props.info.upvotes.includes(firebase.auth().currentUser.uid)) && <img src={upvoteDisabled} style={{ width: '3%' }} alt="Upvote icon (disabled)" onClick={(e) => this.voidVote(e, "up")} />}
                         <p style={{display: 'inline', color: total >= 0 ? 'green' : 'red'}}>{total}</p>
                         {(typeof this.props.info.downvotes === "undefined" || !this.props.info.downvotes.includes(firebase.auth().currentUser.uid)) && <img src={downvote} style={{ width: '3%' }} alt="Downvote icon" onClick={(e) => this.handleVote(e, "down")} />}
-                        {(typeof this.props.info.downvotes !== "undefined" && this.props.info.downvotes.includes(firebase.auth().currentUser.uid)) && <img src={downvoteDisabled} style={{ width: '3%' }} alt="Downvote icon (disabled)" />}
+                        {(typeof this.props.info.downvotes !== "undefined" && this.props.info.downvotes.includes(firebase.auth().currentUser.uid)) && <img src={downvoteDisabled} style={{ width: '3%' }} alt="Downvote icon (disabled)" onClick={(e) => this.voidVote(e, "down")} />}
                         <h3 style={{display: 'inline', 'marginLeft': '3rem', color:'black'}}>{this.props.info.title}</h3>
                         <p style={{display: 'inline', 'float': 'right', color:'black'}}>{this.props.info.author + ", " + this.props.info.date}</p>
                         {this.props.username === this.props.info.author && <p>this will be a delete button</p>}
