ac6bf8c57b52ff00afbcd5da48721b4455ce89c1
xCyziw <neonlight003@gmail.com>
2019-05-01 14:30:09 -0700

    create app bar + install material ui

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -1,28 +1,27 @@
 import React, { Component } from 'react';
-import Navbar from 'react-bootstrap/Navbar'
-import Nav from 'react-bootstrap/Nav'
-import { LinkContainer } from 'react-router-bootstrap';
+import { Link } from 'react-router-dom';
+import AppBar from '@material-ui/core/AppBar';
+import Button from '@material-ui/core/Button';
+import Toolbar from '@material-ui/core/Toolbar';
+import Typography from '@material-ui/core/Typography';
 
 export class Navigation extends Component {
     render() {
+
         return (
-            <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
-                <Navbar.Brand>Project Gravity</Navbar.Brand>
-                <Navbar.Toggle aria-controls="responsive-navbar-nav" />
-                <Navbar.Collapse id="responsive-navbar-nav">
-                    <Nav className="mr-auto">
-                        <LinkContainer exact to="/">
-                            <Nav.Link>Home</Nav.Link>
-                        </LinkContainer>
-                        <LinkContainer to="/contact">
-                            <Nav.Link>About Us</Nav.Link>
-                        </LinkContainer>
-                        <LinkContainer to="/demo">
-                            <Nav.Link>Forum</Nav.Link>
-                        </LinkContainer>
-                    </Nav>
-                </Navbar.Collapse>
-            </Navbar>
+            <AppBar position="static" color="default" className="app-bar">
+        <Toolbar>
+          <Typography variant="h6" color="inherit" noWrap className="app-bar-title">
+            <Link to="/">Gravity</Link>
+          </Typography>
+          <Button component={Link} to="/contact">About</Button>
+          <Button component={Link} to="/demo">Forum</Button>
+          <Button component={Link} to="/">Resources</Button> {/* todo: update link */}
+          <Button color="primary" variant="outlined"> {/* todo: this should open a modal with signup form */}
+            Become a Member
+          </Button>
+        </Toolbar>
+      </AppBar>
         );
     }
 }
