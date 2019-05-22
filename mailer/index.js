ad52e61274e048be57234d89ffd2c291737021d7
jamesbmc <jbmc@uw.edu>
2019-05-22 15:01:30 -0700

    refactor mailer, update to work with new forum path

--- mailer/index.js
+++ mailer/index.js
@@ -48,18 +48,7 @@ app.listen(port, host, () => {
     let pastDay = new Date().getTime() - (24 * 3600 * 1000);
     let postRef = firebase.database().ref('posts').orderByChild('date').startAt(pastDay);
     postRef.on('value', (snapshot) => {
-        let postsObject = snapshot.val();
-        if (postsObject != null) {
-            let postArray = Object.keys(postsObject).map(id => {
-                return {
-                    id: id,
-                    title: postsObject[id].title,
-                }
-            });
-            posts = postArray;
-        } else {
-            posts = [];
-        }
+        posts = getPosts(snapshot)
     });
 
     setInterval(function() {
@@ -73,28 +62,18 @@ app.listen(port, host, () => {
                 if (typeof member.email !== "undefined") {
                     sendEmail(member.email, member.firstName, posts)
                 }
-
-                // Resetting the reference to the database to only get the past day
-                pastDay = new Date().getTime() - (24 * 3600 * 1000);
-                postRef = firebase.database().ref('posts').orderByChild('date').startAt(pastDay);
-                postRef.on('value', (snapshot) => {
-                    let postsObject = snapshot.val();
-                    if (postsObject != null) {
-                        let postArray = Object.keys(postsObject).map(id => {
-                            return {
-                                id: id,
-                                title: postsObject[id].title,
-                            }
-                        });
-                        posts = postArray;
-                    } else {
-                        posts = [];
-                    }
-                });
             });
         } else {
             console.log('no recent posts');
         }
+
+        // Resetting the reference to the database
+        pastDay = new Date().getTime() - (24 * 3600 * 1000);
+        postRef = firebase.database().ref('posts').orderByChild('date').startAt(pastDay);
+
+        postRef.on('value', (snapshot) => {
+            posts = getPosts(snapshot)
+        });
     }, milliseconds_in_day);
 
     console.log(`server is listening at http://${addr}...`);
@@ -109,10 +88,25 @@ async function sendEmail(email, name, posts) {
 
   mailOptions.subject = `Your Project Gravity Daily Digest`;
   let emailHtml = `<p>Hey ${name || ''}! Below are the Project Gravity posts from today:</p>`
-    + posts.map(post => '<br><a href="https://gravity-28052.firebaseapp.com/demo/'+ post.id + '">' + post.title + '</a>').join("") +
+    + posts.map(post => `<br><a href="https://gravity-28052.firebaseapp.com/forum/${post.id}">${post.title}</a>`).join("") +
     '<br><p>Anything sound interesting? <a href="https://gravity-28052.firebaseapp.com/demo/">Join the discussion!</a></p>';
   mailOptions.html = emailHtml;
   await mailTransport.sendMail(mailOptions);
   console.log('email sent to:', email);
   return null;
+}
+
+function getPosts(snapshot) {
+    let postsObject = snapshot.val();
+    if (postsObject != null) {
+        let postArray = Object.keys(postsObject).map(id => {
+            return {
+                id: id,
+                title: postsObject[id].title,
+            }
+        });
+        return postArray;
+    } else {
+        return [];
+    }
 }
