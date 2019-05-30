13eaf7572fb8b35bde81b2621d33e31d6a911f7a
xCyziw <neonlight003@gmail.com>
2019-05-30 14:16:01 -0700

    nav links

--- src/components/OverviewPage.js
+++ src/components/OverviewPage.js
@@ -28,29 +28,29 @@ export class OverviewPage extends Component {
     handleMemberSignUp() {
         // must take away period since firebase doesn't allow them.
         firebase.database().ref("members/set/" + this.state.email.split('.').join("")).once('value')
-        .then((snapshot) => {
-            var a = snapshot.exists();  // true if email exists already, false if doesn't exist.
-            this.setState({emailExists: a});
-        })
-        .then(() => {
-            console.log(this.state.emailExists);
-            if (this.state.fullName === "" || this.state.email === "") {
-                alert("One of the required fields is empty!");
-                return;
-            } else if (this.state.emailExists) {
-                alert("This email is already on our list!");
-            } else {
-                let member = {
-                    email: this.state.email,
-                    firstName: this.state.firstName,
-                    lastName: this.state.lastName,
-                    zip: this.state.zip
-                };
-                this.memberRef.push(member);
-                // use this to check if that email already exists.
-                this.memberRef.child("set").child(this.state.email.split('.').join("")).push(1);
-            }
-        });
+            .then((snapshot) => {
+                var a = snapshot.exists();  // true if email exists already, false if doesn't exist.
+                this.setState({ emailExists: a });
+            })
+            .then(() => {
+                console.log(this.state.emailExists);
+                if (this.state.fullName === "" || this.state.email === "") {
+                    alert("One of the required fields is empty!");
+                    return;
+                } else if (this.state.emailExists) {
+                    alert("This email is already on our list!");
+                } else {
+                    let member = {
+                        email: this.state.email,
+                        firstName: this.state.firstName,
+                        lastName: this.state.lastName,
+                        zip: this.state.zip
+                    };
+                    this.memberRef.push(member);
+                    // use this to check if that email already exists.
+                    this.memberRef.child("set").child(this.state.email.split('.').join("")).push(1);
+                }
+            });
     }
 
     // for member sign in
@@ -81,39 +81,39 @@ export class OverviewPage extends Component {
 
                         <h1 className="landing-header">Together, we can build a better EHR system. Join the movement.</h1>
                         <p>Bringing patients, clinicians, and health care leaders together to create a market demand for robust interoperability between electronic health record (EHR) systems. Add your name for the latest updates.</p>
-                        <Button className="button-style padding-top"  variant="contained" color="secondary" component={Link} to="/contact">
+                        <Button className="button-style padding-top" variant="contained" color="secondary" component={Link} to="/contact">
                             Learn More
                         </Button>
 
                     </Col>
                     <Col xs={10} md={4}>
                         <div className="contain-sign-up">
-                        {/*need to add email signup form component*/}
-                        <form>
-                            <input className="form-item" type="text" name="firstName" placeholder="FIRST NAME" value={this.state.firstName} onChange={event => this.handleChange(event)} />
-                            <input className="form-item" type="text" name="lastName" placeholder="LAST NAME" value={this.state.lastName} onChange={event => this.handleChange(event)}/>
-                            <input className="form-item" type="email" name="email" placeholder="EMAIL" value={this.state.email} onChange={event => this.handleChange(event)}/>
-                            <input className="form-item" type="text" name="zip" placeholder="ZIP / POSTAL CODE" value={this.state.zip} onChange={event => this.handleChange(event)}/>
-                            <Button className="button-style sign-up-button"  variant="contained" color="secondary" onClick={() => this.handleMemberSignUp()}>ADD YOUR NAME</Button>
-                        </form>
+                            {/*need to add email signup form component*/}
+                            <form>
+                                <input className="form-item" type="text" name="firstName" placeholder="FIRST NAME" value={this.state.firstName} onChange={event => this.handleChange(event)} />
+                                <input className="form-item" type="text" name="lastName" placeholder="LAST NAME" value={this.state.lastName} onChange={event => this.handleChange(event)} />
+                                <input className="form-item" type="email" name="email" placeholder="EMAIL" value={this.state.email} onChange={event => this.handleChange(event)} />
+                                <input className="form-item" type="text" name="zip" placeholder="ZIP / POSTAL CODE" value={this.state.zip} onChange={event => this.handleChange(event)} />
+                                <Button className="button-style sign-up-button" variant="contained" color="secondary" onClick={() => this.handleMemberSignUp()}>ADD YOUR NAME</Button>
+                            </form>
                         </div>
                     </Col>
                 </Row>
 
                 <Row className="content-row bg-blue">
-                <Col xsOffset={1} xs={10}>
-                    <p>
-                        The problem of information sharing across different hospitals and healthcare systems is a huge issue in the medical field.
-                        Hospitals use many types of electronic health records (EHR) solutions that do not communicate well with each other, so transmission of patient records
-                        becomes tedious and challenging. Interoperability is a problem that many hospitals face, which is the problem of EHRs from one hospital system
-                        being unable to interact with an EHR from another system. To solve this problem, legislation needs to be created to either mandate a single EHR for all United
-                        States hospitals to use or enforce interoperability standards. We have built a platform to facilitate discussion around the standardization of healthcare record
-                        formats and transmission of those documents. Our goal is creating an accessible repository with information as to why this consolidation is a worthy cause, and
-                        a place to start a discussion on how to achieve mandatory consolidation or interoperability standards through reaching out to state and national legislators.
+                    <Col xsOffset={1} xs={10}>
+                        <p>
+                            The problem of information sharing across different hospitals and healthcare systems is a huge issue in the medical field.
+                            Hospitals use many types of electronic health records (EHR) solutions that do not communicate well with each other, so transmission of patient records
+                            becomes tedious and challenging. Interoperability is a problem that many hospitals face, which is the problem of EHRs from one hospital system
+                            being unable to interact with an EHR from another system. To solve this problem, legislation needs to be created to either mandate a single EHR for all United
+                            States hospitals to use or enforce interoperability standards. We have built a platform to facilitate discussion around the standardization of healthcare record
+                            formats and transmission of those documents. Our goal is creating an accessible repository with information as to why this consolidation is a worthy cause, and
+                            a place to start a discussion on how to achieve mandatory consolidation or interoperability standards through reaching out to state and national legislators.
                 </p>
-                </Col>
+                    </Col>
                 </Row>
-
+                {/*
                 <Row className="content-row">
                 <Col xsOffset={1} xs={10}>
                     <p>
@@ -124,7 +124,7 @@ export class OverviewPage extends Component {
 
                 <Row between="xs" className="content-row bg-blue">
                     <Col xsOffset={1} xs={12} md={3}>
-                    {/*need to add email signup form component*/}
+                    
                     footer column 1
                     </Col>
                     <Col xs={12} md={3}>
@@ -134,6 +134,7 @@ export class OverviewPage extends Component {
                     footer column 3
                     </Col>
                 </Row>
+*/}
             </Grid>
         );
     }
