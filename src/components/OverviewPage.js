51ae4a7eb374563bbeae046c3fb21a3222889d6a
xCyziw <neonlight003@gmail.com>
2019-05-30 14:05:55 -0700

    removed stuff

--- src/components/OverviewPage.js
+++ src/components/OverviewPage.js
@@ -81,13 +81,9 @@ export class OverviewPage extends Component {
 
                         <h1 className="landing-header">Together, we can build a better EHR system. Join the movement.</h1>
                         <p>Bringing patients, clinicians, and health care leaders together to create a market demand for robust interoperability between electronic health record (EHR) systems. Add your name for the latest updates.</p>
-                        <Button  className="button-style" color="primary" variant="contained" component={Link} to="/contact">
+                        <Button className="button-style padding-top"  variant="contained" color="secondary" component={Link} to="/contact">
                             Learn More
                         </Button>
-
-                        Test buttons:
-                        <Button color="primary">Primary</Button>
-                        <Button color="secondary">Secondary</Button>
 
                     </Col>
                     <Col xs={10} md={4}>
@@ -98,13 +94,13 @@ export class OverviewPage extends Component {
                             <input className="form-item" type="text" name="lastName" placeholder="LAST NAME" value={this.state.lastName} onChange={event => this.handleChange(event)}/>
                             <input className="form-item" type="email" name="email" placeholder="EMAIL" value={this.state.email} onChange={event => this.handleChange(event)}/>
                             <input className="form-item" type="text" name="zip" placeholder="ZIP / POSTAL CODE" value={this.state.zip} onChange={event => this.handleChange(event)}/>
-                            <Button className="button-style sign-up-button" variant="outlined" onClick={() => this.handleMemberSignUp()}>ADD YOUR NAME</Button>
+                            <Button className="button-style sign-up-button"  variant="contained" color="secondary" onClick={() => this.handleMemberSignUp()}>ADD YOUR NAME</Button>
                         </form>
                         </div>
                     </Col>
                 </Row>
 
-                <Row className="content-row bg-green">
+                <Row className="content-row bg-blue">
                 <Col xsOffset={1} xs={10}>
                     <p>
                         The problem of information sharing across different hospitals and healthcare systems is a huge issue in the medical field.
